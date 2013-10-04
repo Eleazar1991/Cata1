3f2321760a4560b8bc26b1d7affbe7f23ee75670
Eleazar1991 <eleazar.yio@gmail.com>
2013-10-04 13:55:11 +0100

    Third commit

--- src/cata1/Cata1.java
+++ src/cata1/Cata1.java
@@ -1,4 +1,3 @@
-
 package cata1;
 
 import java.util.Date;
@@ -7,7 +6,7 @@ public class Cata1 {
 
     
     public static void main(String[] args) {
-       Student pepe = new Student("Juan",new Date(91,11,2));
-       System.out.println(pepe.getBirthDate());
+       Student pedro = new Student("Juan",new Date(91,11,2));
+       System.out.println(pedro.getBirthDate());
     }
 }
