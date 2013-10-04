559d9cdd59159515226f763c1f68c951e74eec70
usuario <usuario@10.22.146.197>
2013-10-04 12:48:56 +0100

    Second commit

--- src/cata1/Cata1.java
+++ src/cata1/Cata1.java
@@ -7,7 +7,7 @@ public class Cata1 {
 
     
     public static void main(String[] args) {
-       Student juan = new Student("Juan",new Date(91,11,2));
-       System.out.println(juan.getBirthDate());   
+       Student pepe = new Student("Juan",new Date(91,11,2));
+       System.out.println(pepe.getBirthDate());
     }
 }
